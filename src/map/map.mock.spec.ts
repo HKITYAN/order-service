201ce1bd1fc9fa157c81d38651a3d4ec2d85fd61
WorkingSlave <workig.slave.a@gmail.com>
2020-08-13 20:15:48 +0800

    refractor test cases using loop

--- src/map/map.mock.spec.ts
+++ src/map/map.mock.spec.ts
@@ -1,7 +1,7 @@
 export class MapServiceMock {
     getDistance = async (origin: string, destination: string) : Promise<number> => {
         return new Promise((resolve, reject) => {
-            if (origin[0] < destination[0]) {
+            if (origin[0] === "1") {
                 resolve(200)
             } else {
                 resolve(null)
--- src/order/order.service.spec.ts
+++ src/order/order.service.spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { OrderService } from './order.service';
 import { MapService } from '@/map/map.service';
-import { MapServiceMock } from '@/map/map.mock.spect';
+import { MapServiceMock } from '@/map/map.mock.spec';
 import { getRepository, Repository, UpdateResult } from 'typeorm';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { Order } from './order.entity';
@@ -54,31 +54,37 @@ describe('OrderService', () => {
   });
 
   describe("create order functionality test", () => {
+    const coordinate1: OrderCoordinate = {
+      origin: ["200.4320", "-81.38992"],
+      destination: ["50.4320", "-81.38992"]
+    }
+    const coordinate2: OrderCoordinate = {
+      origin: ["100.4320", "-81.38992"],
+      destination: ["50.4320", "-81.38992"]
+    }
 
-    it("able to return a new order", async () => {
-      const coordinate: OrderCoordinate = {
-        origin: ["41.4320", "-81.38992"],
-        destination: ["50.4320", "-81.38992"]
-      }
+    test.each`
+      input          |  case
+      ${coordinate1} |  ${"return a new order"}
+      ${coordinate2} |  ${"handle exception FAIL_TO_GET_DISTANCE"}
+    `("createOrder => $case", async ({ input }) => {
+      
       jest.spyOn(orderRepo, "save").mockImplementation(() => Promise.resolve(unassignedOrder))
-      expect(await orderService.createOrder(coordinate)).toBe(unassignedOrder);
-    })
 
-    it("able to handle exception FAIL_TO_GET_DISTANCE", async () => {
-      const coordinate: OrderCoordinate = {
-        origin: ["60.4320", "-81.38992"],
-        destination: ["50.4320", "-81.38992"]
+      expect.assertions(1);
+      try {
+        expect(await orderService.createOrder(input)).toBe(unassignedOrder);
+      } catch(exception) {
+        const httpException : HttpException = new HttpException("FAIL_TO_GET_DISTANCE", HttpStatus.INTERNAL_SERVER_ERROR)
+        expect(exception).toMatchObject(httpException)
       }
-
-      const httpException : HttpException = new HttpException("FAIL_TO_GET_DISTANCE", HttpStatus.INTERNAL_SERVER_ERROR)
-
-      await expect(orderService.createOrder(coordinate)).rejects.toThrow(httpException);
     })
   });
 
   describe("take order functionality test", () => {
-    const unassingedOrderId = "c22a7249-f252-4dee-9cbc-735bac0f79b2"
-    const takenOrderId = "3ffc6389-a7b0-4f08-a7c2-56100592f630"
+    const unassingedOrderId = unassignedOrder.id
+    const takenOrderId = takenOrder.id
+    const fakeOrderId = "xxxx-xxxx-xxx"
 
     const findOne = (id) => {
       if (id === unassingedOrderId) return Promise.resolve(unassignedOrder)
@@ -86,34 +92,44 @@ describe('OrderService', () => {
       return null;
     }
     
-
-    it("able to take order successfully", async () => {
+    test.each`
+      input                   |   case
+      ${unassingedOrderId}    |   ${"take an order with UNASSIGNED status"}
+      ${takenOrderId}         |   ${"take an order with TAKEN status"}
+      ${fakeOrderId}          |   ${"take an order which does not exists"}
+    `("takeOrder => $case", async ({ input }) => {
       const successResponse : UpdateStatus = { status: "SUCCESS" }
       jest.spyOn(orderRepo, "findOne").mockImplementation(findOne)
       jest.spyOn(orderRepo, "update").mockImplementation(() => Promise.resolve({ affected: 1 } as UpdateResult))
-      expect(await orderService.takeOrder(unassingedOrderId)).toMatchObject(successResponse);
-    })
-
-    it("able to handle exception ORDER_NOT_FOUND", async () => {
-      const httpException = new HttpException("ORDER_NOT_FOUND", HttpStatus.NOT_FOUND)
-      jest.spyOn(orderRepo, "findOne").mockImplementation(findOne)
-      await expect(orderService.takeOrder("xxxx-xxxx-xxxx")).rejects.toThrow(httpException);
-    })
-
-    it("able to handle exception ORDER_ALREADY_TAKEN", async () => {
-      const httpException = new HttpException("ORDER_ALREADY_TAKEN", HttpStatus.FORBIDDEN)
-      jest.spyOn(orderRepo, "findOne").mockImplementation(findOne)
-      await expect(orderService.takeOrder(takenOrderId)).rejects.toThrow(httpException);
+      
+      try {
+        expect(await orderService.takeOrder(input)).toMatchObject(successResponse);
+      } catch(exception) {
+        const orderTakenException = new HttpException("ORDER_ALREADY_TAKEN", HttpStatus.FORBIDDEN)
+        const orderNotFoundException = new HttpException("ORDER_NOT_FOUND", HttpStatus.NOT_FOUND)
+        switch(input) {
+          case takenOrderId:
+            expect(exception).toMatchObject(orderTakenException)
+            break
+          case fakeOrderId:
+            expect(exception).toMatchObject(orderNotFoundException)
+            break
+          default:
+            expect(exception).toMatchObject(orderNotFoundException) 
+        }
+          
+        
+      }
     })
   })
 
   describe("listOrder functionality test", () => {
-    it ("able to list order when there exists orders", async () => {
+    it ("listOrder => list order when there exists orders", async () => {
       jest.spyOn(orderRepo, "find").mockImplementation(() => Promise.resolve([takenOrder, unassignedOrder]))
       expect(await orderService.listOrder(1, 1)).toEqual(expect.arrayContaining([takenOrder, unassignedOrder]))
     })
 
-    it ("able to return empty array when there are no orders", async () => {
+    it ("listOrder => return empty array when there are no orders", async () => {
       jest.spyOn(orderRepo, "find").mockImplementation(() => Promise.resolve([]))
       expect(await orderService.listOrder(1, 1)).toEqual(expect.arrayContaining([]))
     })
